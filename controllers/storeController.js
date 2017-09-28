0015b6a97457b50727c487940ef9692cca4a764d
Paul Walsh <twentytwentyk@live.com>
2017-09-28 12:27:36 -0400

    first

---
+++ controllers/storeController.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+exports.homePage = (req, res) => {
+  console.log(req.name);
+  res.render('index');
+};
+
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store 🙎‍♂️' });
+};
+
+
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+  res.redirect('/');
+}
